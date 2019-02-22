66b1314d78f308cd9ae84417be16aa4892cd29b9
iwanbazz <iwan.bazz@gmail.com>
2019-02-23 00:44:42 +0700

    Change base_url

--- api/application/config/config.php
+++ api/application/config/config.php
@@ -21,9 +21,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
-|
+|$config['base_url'] = 'http://localhost/rental_mobil/api';
 */
-$config['base_url'] = 'http://localhost/rental_mobil/api';
+
+$base_root  = "http://".$_SERVER['HTTP_HOST'];
+$base_root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $base_root;
 
 /*
 |--------------------------------------------------------------------------
--- application/config/config.php
+++ application/config/config.php
@@ -21,9 +21,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
-|
+|$config['base_url'] = 'http://localhost/rental_mobil/';
 */
-$config['base_url'] = 'http://localhost/rental_mobil/';
+
+$base_root  = "http://".$_SERVER['HTTP_HOST'];
+$base_root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $base_root;
 
 /*
 |--------------------------------------------------------------------------
@@ -64,7 +67,7 @@ $config['uri_protocol']	= 'REQUEST_URI';
 |
 | http://codeigniter.com/user_guide/general/urls.html
 */
-$config['url_suffix'] = '.html';
+$config['url_suffix'] = '';
 
 /*
 |--------------------------------------------------------------------------
