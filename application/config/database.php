4aee792dc621ddae12c85b82f9de11eba2e556ae
iwanbazz <iwan.bazz@gmail.com>
2019-03-02 01:24:45 +0700

    Change db to remotemysql

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'rentalmobil',
+	'hostname' => 'remotemysql.com',
+	'username' => 'BdRSEx5rtV',
+	'password' => 'gR8qeX6PNf',
+	'database' => 'BdRSEx5rtV',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
