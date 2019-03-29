08a7b4be65f0df0ddc6de2d4ff0cfe1311f214f6
iwanbazz <iwan.bazz@gmail.com>
2019-03-29 15:41:24 +0700

    Clean indent & monir view fixed - Fasilitas

--- application/views/fasilitas/tb_fasilitas_read.php
+++ application/views/fasilitas/tb_fasilitas_read.php
@@ -1,32 +1,24 @@
-<!-- Content Wrapper. Contains page content -->
-
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>Halaman Fasilitas Mobil <small>form data Fasilitas Mobil</small></h1>
-        </section><!-- Main content -->
-        <section class="content">
-            <div class="row">
-                <!-- left column -->
-                <div class="col-md-12">
-                    <div class="box box-info">
-
-
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Detail <?php echo $FASILITAS ?></h3>
-                        </div><!-- /.box-header -->
-                        
-                        <!-- form start -->
-                        <div class="box-body">
-                            <table class="table">
-                                <tr><td>FASILITAS</td><td><?php echo $FASILITAS; ?></td></tr>
-                                <tr><td>KET FASILITAS</td><td><?php echo $KET_FASILITAS; ?></td></tr>
-                                <!-- <tr><td>BIAYA</td><td><?php echo $BIAYA; ?></td></tr> -->
-                                <tr><td></td><td><a href="<?php echo site_url('fasilitas') ?>" class="btn btn-default">Cancel</a></td></tr>
-                            </table>
-                        </div>                        
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>Halaman Fasilitas Mobil</h1>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-info">
+                    <div class="box-header with-border">
+                        <h2 class="box-title">Detail <?php echo $FASILITAS ?></h2>
                     </div>
-                </div><!--/.col (right) -->
+                    <div class="box-body">
+                        <table class="table">
+                            <tr><td>FASILITAS</td><td><?php echo $FASILITAS; ?></td></tr>
+                            <tr><td>KET FASILITAS</td><td><?php echo $KET_FASILITAS; ?></td></tr>
+                            <!-- <tr><td>BIAYA</td><td><?php echo $BIAYA; ?></td></tr> -->
+                            <tr><td></td><td><a href="<?php echo site_url('fasilitas') ?>" class="btn btn-default">Cancel</a></td></tr>
+                        </table>
+                    </div>                        
+                </div>
             </div>
-        </section><!-- /.content -->
-    </div><!-- /.content-wrapper -->
+        </div>
+    </section>
+</div>
